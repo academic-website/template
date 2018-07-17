6345ce342393fae2665502003f3c3832e865d546
iPadisthin <albert.penticoss@gmail.com>
2018-07-17 14:58:36 +1000

    tickets

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var offset = $('.main-nav').offset();
+    var offsetTrigger = offset.top;
+    $(window).scroll(function() {
+        if( $(this).scrollTop() > offsetTrigger )  {
+            $('.main-nav').addClass('fixed');
+        }
+        else {
+            $('.main-nav').removeClass('fixed');
+        }
+    });
+});
