40636a2f8c70169916a85e88da6d54fe6cfc7de5
iPadisthin <albert.penticoss@gmail.com>
2018-07-20 15:16:07 +1000

    responsive tickets

--- js/main.js
+++ js/main.js
@@ -13,4 +13,17 @@ $(document).ready(function() {
         offset = $('.main-nav').offset();
         offsetTrigger = offset.top;
     });
+
+    /* bio in modal */
+    $('.show-bio').on('click', function() {
+        $('.overlay').addClass('on');
+        $('body').addClass('noScroll');
+        $(this).prev().addClass('modal').show();
+    });
+    $('.overlay, .bio').on('click', function() {
+        $('.overlay').removeClass('on');
+        $('body').removeClass('noScroll');
+        $('.bio').removeClass('modal').hide();
+    });
+    /* end bio in modal */
 });
