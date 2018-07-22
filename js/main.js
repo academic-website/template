af97bc98d7b54b69e075617fb330b831bcd7c393
albert.penticoss@gmail.com <albert.penticoss@gmail.com>
2018-07-22 01:54:37 +0000

    Deploy from Forestry.io

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
 $(document).ready(function() {
     var offset = $('.main-nav').offset();
     var offsetTrigger = offset.top;
+    var navHeight = $('.main-nav').height();
     $(window).scroll(function() {
         if( $(this).scrollTop() > offsetTrigger )  {
             $('.main-nav').addClass('fixed');
+            $('main').css('padding-top', navHeight);
         }
         else {
             $('.main-nav').removeClass('fixed');
+            $('main').css('padding-top', 'initial');
         }
     });
     $(window).resize(function() {
         offset = $('.main-nav').offset();
         offsetTrigger = offset.top;
+        navHeight = $('.main-nav').height();
     });
 
     /* bio in modal */
     $('.show-bio').on('click', function() {
         $('.overlay').addClass('on');
         $('body').addClass('noScroll');
-        $(this).prev().addClass('modal').show();
+        $(this).prev().find('.bio').addClass('modal').show();
     });
     $('.overlay, .bio').on('click', function() {
         $('.overlay').removeClass('on');
